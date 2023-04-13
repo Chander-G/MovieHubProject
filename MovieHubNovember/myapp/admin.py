75d602f53f5edf8d4ef09da0cff362fe7945be89
Chander G <chanderg009@gmail.com>
2023-04-13 18:05:28 +0530

    creating movie hub projects with two models

---
+++ MovieHubNovember/myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from myapp.models import Genres,Movies
+admin.site.register(Genres)
+admin.site.register(Movies)
+
+#python manage.py createsuperuser
