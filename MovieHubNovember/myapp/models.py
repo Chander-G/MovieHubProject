12ff6465ea2dce6496f080ec5da110ab51f820f6
Chander G <chanderg009@gmail.com>
2023-04-14 09:52:20 +0530

    added  new model reviews

--- MovieHubNovember/myapp/models.py
+++ MovieHubNovember/myapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Genres(models.Model):
     genre=models.CharField(max_length=120,unique=True)
@@ -23,8 +23,15 @@ class Movies(models.Model):
     poster_image=models.ImageField(upload_to='images',null=True,blank=True)
     description=models.CharField(max_length=200,null=True)
 
+
     def __str__(self):
         return self.name
+
+class Reviews(models.Model):
+    movies=models.ForeignKey(Movies,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    comment=models.CharField(max_length=200)
+    rating=models.PositiveIntegerField()
     
 
 #data redundancy
